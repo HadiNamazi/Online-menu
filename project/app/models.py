7ec76d230e29d01b843c7b592451cdebc44dad5e
Hadi <hadi.namazizadegan@gmail.com>
2024-10-13 00:23:40 +0330

    models created

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    primary_price = models.IntegerField()
+    secondary_price = models.IntegerField()
+    image = models.ImageField(upload_to ='item_images/')
+    description = models.TextField()
+    rate = models.FloatField()
+    likes = models.IntegerField()
+    status = models.CharField(max_length=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def menu(req):
+def menu(req, table):
     return render(req, 'app/menu.html', {})
