b9ae199b7c193d51809de3b5da05a09cb08719e7
Hadi <hadi.namazizadegan@gmail.com>
2024-10-12 23:39:16 +0330

    menu.html added

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(req):
-    return HttpResponse('kiumars')
+def menu(req):
+    return render(req, 'app/menu.html', {})
