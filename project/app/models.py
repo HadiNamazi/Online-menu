1e4cd40468b2ff2a8aa78529a163591f9b702303
Hadi <hadi.namazizadegan@gmail.com>
2024-10-29 00:35:16 +0330

    GeneralInfo added

--- project/app/models.py
+++ project/app/models.py
@@ -20,3 +20,11 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+    
+class GeneralInfo(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    phone_num = models.CharField(max_length=20)
+    logo = models.ImageField(upload_to ='theme_images/')
+    header_img = models.ImageField(upload_to ='theme_images/')
+    color = models.CharField(max_length=10) #hex
--- project/app/views.py
+++ project/app/views.py
@@ -4,8 +4,10 @@ from . import models
 def menu(req, table):
     items = models.Item.objects.all()
     categories = models.Category.objects.all()
+    general_infos = models.GeneralInfo.objects.get()
     context = {
         'items': items,
         'categories': categories,
+        'general_infos': general_infos,
     }
     return render(req, 'app/menu.html', context)
